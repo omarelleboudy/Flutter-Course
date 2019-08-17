9d8d1f208f0c0721411144a4efc1d2436d934483
unknown <omarelleboudy@gmail.com>
2019-08-18 00:41:21 +0200

    10th

--- Clima-Flutter/lib/services/location.dart
+++ Clima-Flutter/lib/services/location.dart
@@ -1,11 +1,15 @@
 import 'package:geolocator/geolocator.dart';
 
 class Location {
-  void getCurrentLocation() async {
+  double latitude;
+  double longitude;
+
+  Future<void> getCurrentLocation() async {
     try {
       Position position = await Geolocator()
           .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
-      print(position);
+      latitude = position.latitude;
+      longitude = position.longitude;
     } catch (e) {
       print(e);
     }
